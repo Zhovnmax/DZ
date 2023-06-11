7d9f74bd952ee1de2c126c7051dad51e93828342
Zovnmax <zhovnmax@gmail.com>
2023-06-11 15:29:33 +0300

    Add new branch

--- Git_hub/Git_hub/Source.cpp
+++ Git_hub/Git_hub/Source.cpp
@@ -6,13 +6,20 @@ int  Suma(int a, int b)
 
 	return a + b;
 }
+
+int Sered_Ar(int a, int b)
+{
+	return (a + b) / 2;
+}
 int main()
 {
 	int a, b;
 
 	cout << "Enter a, b:" << endl;
-	cin >> a, b;
+	cin >> a >> b;
 	cout << Suma(a, b);
+
+	cout << Sered_Ar(a, b);
 
 
 	return 0;
